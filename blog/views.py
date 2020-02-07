04f401981f162e1547296e8a800b04d3b27501e8
7se1rk <nase0217@gmail.com>
2020-02-07 13:12:14 +0900

    commit on "master"

--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,9 @@ def home(request):
     return HttpResponse('<h1>hello world</h1>')
 
 def login(request):
-    return render(request, 'blog/login.html')
+    return render(request, 'html/login.html')
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    return render(request, 'html/post_list.html', {'posts':posts})
 
